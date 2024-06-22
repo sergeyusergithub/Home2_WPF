d109ce62faa393255f7bf4a2cfb30b84d51e8a2e
Sergey Sergey <sergeymailnew@mail.ru>
2024-06-22 22:42:03 +0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Home2_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
